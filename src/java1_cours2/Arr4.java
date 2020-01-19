3ef65bcf5078d24aae9b36935558d90e7484fdc0
Alekcgit <59404139+Alekcgit@users.noreply.github.com>
2020-01-19 08:23:25 +0100

    Удаление папок и файлов

---
+++ src/java1_cours2/Arr4.java
@@ -0,0 +1,18 @@
+public class Arr4 {
+    public static void main(String[] args) {
+        int counter=0;
+        int[][] table=new int[4][4];
+        for (int i=0; i < 4; i++)  {
+          for (int j=0; j < 4; j++)  {
+               if (i==j || j==3-i)  {
+          table[i][j]=1;
+ System.out.print(table[i][j]+" ");
+          } else {
+           table[i][j]=counter;
+            System.out.print(table[i][j]+" ");
+                }
+            }
+System.out.println();
+}
+    }
+}
