34eca8a96705b34a5e024b31b69030d5422c8aac
Alekcgit <59404139+Alekcgit@users.noreply.github.com>
2020-01-19 16:01:17 +0100

    Домашнее задание 2.0

---
+++ src/java1_cours2/ArrN.java
@@ -0,0 +1,15 @@
+
+public class ArrN {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 3, 2, 10, 4, 4, 20};
+        arrNp(arr);
+       int n=2;
+    }
+    static void arrNp (arr n) {
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i]+" ");
+              
+    }
+    System.out.println(n);  
+        }
+}
---
+++ src/java1_cours2/ArrSumLR.java
@@ -0,0 +1,28 @@
+
+public class ArrSumLR {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 3, 2, 10, 4, 4, 21};
+        sumArr(arr);
+    }
+        static int[] sumArr (int[] arr) {
+        boolean bolt=true, bolf=false;
+        int arrit = 0, arrlef = 0, arrez = 0;
+            for (int i = 0; i < arr.length; i++) {
+                arrlef += arr[i];
+                if (i == arr.length - 3) {
+                    System.out.println(arrlef + " arrlef");
+                    arrez = arrlef;
+                }
+                if (i > arr.length - 3) {
+                    arrit += arr[i];
+                }
+            }
+            System.out.println(arrit + " arrit");
+            if (arrez == arrit) {
+                System.out.println(bolt);
+            } else {
+                System.out.println(bolf);
+            }
+            return arr;
+        }
+}
