f0c5344c818e2f91d4a58ecc1e7a2117b421219d
Alekcgit <59404139+Alekcgit@users.noreply.github.com>
2020-01-22 19:55:13 +0100

    Переделал 5 задание 2 урока

--- src/java1_cours2/MaxMin.java
+++ src/java1_cours2/MaxMin.java
@@ -1,26 +1,19 @@
-package java1_cours2;
-// Задание 5
+//Урок 2 Задание 5
 public class MaxMin {
     public static void main(String[] args) {
-        int[] arr = { 9, 5, 3, 2, 11, 4, 5, 11, 3, 8, 9, 7};
-        int[] arrmi = new int[1];
-        int[] arrma= new int[1];
+        int[] arr = { 9, 5, 3, 2, 11, 4, 5, 11, 3, 8, 9, 7 };
+        int arrmi = arr[0];
+        int arrma = arr[0];
         for (int i = 0; i < arr.length; i++) {
-      if (arr[i] <= arr[arr.length - 1]) {
-                arrmi[0] = arr[i];
-            } else {
-                arrma[0] = arr[i];    
-            }       
+            if (arr[i] < arrmi) {
+                arrmi = arr[i];
+            }
+            if (arr[i] > arrma) {
+                arrma = arr[i];
+            }
         }
-       for (int i = 0; i < arr.length; i++) {
-        if (arrmi[0] >= arr[i])  {
-            arrmi[0] = arr[i];
-        }   
-        if (arrma[0] <= arr[i])  {
-            arrma[0] = arr[i];
-        }  
-       }
-       System.out.println(arrma[0]);
-     System.out.println(arrmi[0]);          
+        System.out.println("---------");
+        System.out.println("Минимальное число в массиве arrmi = " + arrmi);
+        System.out.println("Максимальное число в массиве arrma = " + arrma);
     }
 }
