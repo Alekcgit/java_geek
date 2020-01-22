673573704345689e2f8ecf56bd41d5a4d81fc887
Alekcgit <59404139+Alekcgit@users.noreply.github.com>
2020-01-22 14:19:04 +0100

    Задание 6 переделал

--- src/java1_cours2/ArrSumLR.java
+++ src/java1_cours2/ArrSumLR.java
@@ -1,29 +1,37 @@
-package java1_cours2;
-// Задание 6
+
 public class ArrSumLR {
     public static void main(String[] args) {
-        int[] arr = {1, 5, 3, 2, 10, 4, 4, 21};
-        sumArr(arr);
+        System.out.println(checkSum(new int[] { 2, 2, 2, 1, 2, 2, 10, 1 }));
+        System.out.println(checkSum(new int[] { 3, 3 }));
+        System.out.println(checkSum(new int[] { 8 }));
+        System.out.println(checkSum(new int[] { 6, 7, 8 }));
+        System.out.println(checkSum(new int[] { 6, 7, 8, 5 }));
+        System.out.println(checkSum(new int[] { 1, 1, 1, 2, 1 }));
+        System.out.println(checkSum(new int[] { 1, 5, 3, 2, 10, 4, 4, 20 }));
     }
-        static int[] sumArr (int[] arr) {
-        boolean bolt=true, bolf=false;
-        int arrit = 0, arrlef = 0, arrez = 0;
-            for (int i = 0; i < arr.length; i++) {
-                arrlef += arr[i];
-                if (i == arr.length - 3) {
-                    System.out.println(arrlef + " arrlef");
-                    arrez = arrlef;
-                }
-                if (i > arr.length - 3) {
-                    arrit += arr[i];
-                }
+
+    private static boolean checkSum(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int leftSum = sum(array, 0, i);
+            int rightSum = sum(array, i, array.length);
+            if (leftSum == rightSum) {
+                System.out.println("---------");
+                System.out.println("Колличество элементов в массиве " + array.length);
+                System.out.println("Cумма равна " + leftSum + "; Колличество элементов с левой стороны " + i);
+                return true;
             }
-            System.out.println(arrit + " arrit");
-            if (arrez == arrit) {
-                System.out.println(bolt);
-            } else {
-                System.out.println(bolf);
-            }
-            return arr;
         }
+        System.out.println("---------");
+        System.out.println("Колличество элементов в массиве " + array.length);
+        System.out.println("Сумма неравна");
+        return false;
+    }
+
+    private static int sum(int[] array, int start, int end) {
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
 }
