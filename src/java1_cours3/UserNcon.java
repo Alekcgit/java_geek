5bdc3a1528dae08ee1613285757e6bed0f9e6603
Alekcgit <59404139+Alekcgit@users.noreply.github.com>
2020-01-26 20:33:15 +0100

    Так же изменил и UserNcon

--- src/java1_cours3/UserNcon.java
+++ src/java1_cours3/UserNcon.java
@@ -1,76 +1,47 @@
 import java.util.Random;
 import java.util.Scanner;
+
 // Урок 3 Задание 1
 public class UserNcon {
-    public static Scanner sc = new Scanner(System.in);
+    private static final int NUMBER_ATTEMPT = 3;
+    private static final int NUMBER_MAX = 9;
 
     public static void main(String[] args) {
-        Random rand = new Random();
-        int r = rand.nextInt(5);
-        int d = getNumberFromScanner("Введите целое число в пределах от 0 до 5", 0, 5);
-        int zip = 1;
-        int cont = 1;
-        while (zip == 1) {
-            while (d > r) {
-                System.out.println("Попытка " + cont++);
-                System.out.println("Загаданное число меньше");
-                d = getNumberFromScanner("Введите целое число в пределах от 0 до 5", 0, 5);
-                if (cont == 3 && d != r) {
-                    System.out.println("Три попытки. Игра сначала");
-                    r = rand.nextInt(5);
-                    d = getNumberFromScanner("Введите целое число в пределах от 0 до 5", 0, 5);
-                    cont = 1;
-                }
-            }
-            while (d < r) {
-                System.out.println("Попытка " + cont++);
-                System.out.println("Загаданное число Больше");
-                d = getNumberFromScanner("Введите целое число в пределах от 0 до 5", 0, 5);
-                if (cont == 3 && d != r) {
-                    System.out.println("Три попытки. Игра сначала");
-                    r = rand.nextInt(5);
-                    d = getNumberFromScanner("Введите целое число в пределах от 0 до 5", 0, 5);
-                    cont = 1;
-                }
-                while (d > r) {
-                    if (cont == 3 && d != r) {
-                        System.out.println("Три попытки. Игра сначала");
-                        r = rand.nextInt(5);
-                        d = getNumberFromScanner("Введите целое число в пределах от 0 до 5", 0, 5);
-                        cont = 1;
-                    }
-                    System.out.println("Попытка " + cont++);
-                    System.out.println("Загаданное число меньше");
-                    d = getNumberFromScanner("Введите целое число в пределах от 0 до 5", 0, 5);
-                }
-            }
-            if (d == r) {
-                System.out.println("Числа равны");
-            }
-            if (cont == 3) {
-                System.out.println("Три попытки. Игра сначала");
-                d = getNumberFromScanner("Введите целое число в пределах от 0 до 5", 0, 5);
-            }
-            Scanner z = new Scanner(System.in);
-            System.out.println("Продолжить игру -1 Закончить -0");
-            zip = z.nextInt();
-            if (zip == 0) {
-                System.out.println("Игра закончена");
-            }
-            if (zip == 1) {
-                System.out.println("Игра продолжена");
-                d = getNumberFromScanner("Введите целое число в пределах от 0 до 5", 0, 5);
+        Random random = new Random();
+        int numberTarget = random.nextInt(NUMBER_MAX + 1);
+        System.out.println(
+                "Я загадал число от 0 до " + NUMBER_MAX + ". Угадай, какое? \nЧисло попыток " + NUMBER_ATTEMPT);
+        Scanner scanner = new Scanner(System.in);
+        for (int attempt = 0; attempt < NUMBER_ATTEMPT; attempt++) {
+            System.out.printf("Попытка номер %d %n", attempt + 1);
+            int userNumber = gerUserNumber(scanner);
+            if (userNumber == numberTarget) {
+                System.out.println("Верно! Вы отгадали! Это число " + numberTarget);
+                break;
+            } else if (userNumber < numberTarget) {
+                System.out.println("Загаданное число больше " + userNumber);
+            } else {
+                System.out.println("Загаданное число меньше " + userNumber);
             }
         }
-        sc.close();
     }
 
-    public static int getNumberFromScanner(String message, int min, int max) {
-        int x;
+    private static int gerUserNumber(Scanner scanner) {
+        int result = -1;
         do {
-            System.out.println(message);
-            x = sc.nextInt();
-        } while (x < min || x > max);
-        return x;
+            try {
+                System.out.println("Введите число: ");
+                String consoleLine = scanner.nextLine();
+                int value = Integer.parseInt(consoleLine);
+                if (value < 0 || value > NUMBER_MAX) {
+                    System.out.println("Число вне диапазона, пробуй еще!");
+                    continue;
+                }
+                result = value;
+            } catch (NumberFormatException e) {
+                System.out.println("Вводить можно только число!");
+            }
+        } while (result == -1);
+        return result;
     }
 }
