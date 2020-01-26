d79b082474c455f003c5ea651ef539479749cf8a
Alekcgit <59404139+Alekcgit@users.noreply.github.com>
2020-01-26 20:32:15 +0100

    Переделал

--- src/java1_cours3/ArrWord.java
+++ src/java1_cours3/ArrWord.java
@@ -1,46 +1,42 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+
 // Урок 3 Задание 2
 public class ArrWord {
+  public static final int FAKE_WORD_LENGTH = 15;
+  public static final String[] WORDS = new String[] { "apple", "orange", "lemon", "banana", "apricot", "avocado",
+      "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
+      "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato" };
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String[] words = { "apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
-        "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
-        "pepper", "pineapple", "pumpkin", "potato" };
-    Random rand = new Random();
-    int r = rand.nextInt(words.length - 1);
-   // System.out.println("Длина массива words " + words.length);
-    //System.out.println("Случайное число " + r);
-    System.out.println("---------");
-    System.out.println("Введите слово");
-    String wor = words[r];
-    int lin = wor.length();
-    //System.out.println(lin);
-    char a = wor.charAt(0);
-    char b = wor.charAt(1);
-  // System.out.println(wor);
-    String wor1 = (sc.next()).toLowerCase();
-    int i = 0;
-    int d1 = 0;
-    while (i < lin) {
-      if (wor.equals(wor1)) {
-      System.out.println("Верно");
-      System.out.println(wor.equals(wor1));
-        i = lin;
-      } else {
-        System.out.println("не верно");
-        System.out.println(wor.equals(wor1));
-        System.out.println("---------");
-        i++;
-        for (d1 = 0; d1 < i; d1++) {
-          System.out.print(wor.charAt(d1));
+    Random random = new Random();
+    int index = random.nextInt(WORDS.length);
+    String targerWord = WORDS[index];
+    String userWord = null;
+    Scanner scanner = new Scanner(System.in);
+    do {
+      System.out.println("---------");
+      System.out.println("Угадайте слово: ");
+      userWord = scanner.nextLine();
+      if (isWin(targerWord, userWord)) {
+        System.out.printf("Верно! Это слово %s%n", targerWord);
+        break;
+      }
+      System.out.println("Не верно! Ниже тебе подсказка.");
+      for (int i = 0; i < FAKE_WORD_LENGTH; i++) {
+        char chr = '#';
+        if (i < userWord.length() && i < targerWord.length() && userWord.charAt(i) == targerWord.charAt(i)) {
+          chr = userWord.charAt(i);
         }
-        System.out.println();
-        wor1 = (sc.next()).toLowerCase();
-      } 
-    }
-    System.out.println("Верно"); 
-    sc.close();
+        System.out.print(chr);
+      }
+      System.out.println();
+    } while (true);
+    System.out.println("Игра окончена");
+  }
+
+  private static boolean isWin(String targetWord, String userWord) {
+    return targetWord.equals(userWord);
   }
 }
